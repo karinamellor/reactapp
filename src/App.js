10ef4bf7e40468142dc26b55ede689a296477286
Karina <karinamello.d@gmail.com>
2023-10-03 23:06:23 +0000

    Add google fonts and CSS.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
+import style from "./App.module.css";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
      <NavBar />
-         
-    </div>
-  );
+   </div>
+ );
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,32 @@
-import React from 'react'
-import { Navbar, Container, Nav, NavbarBrand } from 'react-bootstrap';
-import logo from "../assets/gluten free logo.png";
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/freelogo.png";
 
 
 const NavBar = () => {
- return (
-  <Navbar expand="md" fixed="top">
-    <Container>
-           <Navbar.Brand>
-              <img src={logo} alt="logo" height="50" />
-           </Navbar.Brand> 
-         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-         <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                   <Nav.Link>Home</Nav.Link>
-                   <Nav.Link>Sign in</Nav.Link>
-                   <Nav.Link>Sign up</Nav.Link>
-               </Nav>  
-           </Navbar.Collapse>
-       </Container>
-  </Navbar>
- );
+  return (
+     <Navbar expand="md" fixed="top">
+         <Container>
+             <Navbar.Brand>
+                 <img src={logo} alt="logo" height="45" />
+              </Navbar.Brand> 
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                   <Nav className="ml-auto text-left">
+                        <Nav.Link>
+                             <i className="fas fa-home"></i>Home
+                        </Nav.Link>
+                         <Nav.Link>
+                             <i className="fas fa-sign-in-alt"></i>Sign in
+                         </Nav.Link>
+                         <Nav.Link>
+                             <i className="fas fa-user-plus"></i>Sign up
+                         </Nav.Link>
+                    </Nav>  
+              </Navbar.Collapse>
+           </Container>
+       </Navbar>
+   );
 };
-
 
 export default NavBar;
